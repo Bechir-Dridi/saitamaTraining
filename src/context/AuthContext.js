6d603203b1b8d2586c2b1057b19249b3ca22f5d8
Bechir-Dridi <bechir7dridi#gmail.com>
2023-11-03 19:40:37 +0100

    fix route01

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -44,7 +44,7 @@ export const AuthContextProvider = ({ children }) => {
         if (user) {
             dispatch({ type: "LOGIN", payload: user })
         }
-    }, [state])
+    }, [user])
 
     //B2.
     console.log("AuthContext state:", state)
