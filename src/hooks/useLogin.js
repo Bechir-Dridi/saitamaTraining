6e1c34d605efd54b92efbf36b34506702a85999d
Bechir-Dridi <bechir7dridi#gmail.com>
2024-05-29 21:46:17 +0100

    console.log of email and password removed from useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -11,8 +11,6 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        console.log("json:", email, password);
-
         const response = await fetch("https://saitama-server.onrender.com/api/user/login", {
             //I didn't mention localhost 3000 because in package.json,
             //I added this=>  "proxy": "http://localhost:4000",
